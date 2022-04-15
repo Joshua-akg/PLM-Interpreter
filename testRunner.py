af65279c516ef08bfe007501c547301e36a80f5d
Joshua Akingbade <gbengaakingbade3@gmail.com>
2022-04-15 15:48:11 +0100

    Added error catching for functions

---
+++ testRunner.py
@@ -0,0 +1,24 @@
+from os import listdir
+import os
+
+PATH = './'
+FILENAME = 'Assignment.jj'
+TESTPATH = './tests'
+
+
+def main():
+    cmd = f'javacc {FILENAME} && javac *.java'
+    os.system(cmd)
+
+    tests = os.listdir(TESTPATH)
+    print(f'Detected {len(tests)} tests in {TESTPATH}')
+    for count, test in enumerate(tests, 1):
+        print(
+            f'------------------------------------\nRUN TEST: {test} [{count}/{len(tests)}]')
+        cmd = f'(java {FILENAME[:-3]} < {TESTPATH}/{test})'
+        os.system(cmd)
+        print('------------------------------------')
+
+
+if __name__ == '__main__':
+    main()
